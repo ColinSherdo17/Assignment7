7f940bd0c8068e005e60e1adc3d6891237086934
Colin Sheridan <C.Sheridan24@nuigalway.ie>
2020-03-19 16:20:22 +0000

    Removed the Index in Access. This sorted the Duplicate Error. However, I have no Primary Key now

---
+++ DbAssignment6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Data;
+using System.Data.OleDb;
+
+namespace DbAssignment6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OleDbConnection connect = new OleDbConnection();
+            connect.ConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\Owner\Desktop\COLLEGE\.NET\Veichles.accdb";
+            connect.Open();
+
+            OleDbCommand command = new OleDbCommand();
+            command.Connection = connect;
+            command.CommandText = "SELECT Student_ID FROM Veichles";
+            OleDbDataReader reader = command.ExecuteReader();
+            connect.Close();
+        }
+    }
+}
