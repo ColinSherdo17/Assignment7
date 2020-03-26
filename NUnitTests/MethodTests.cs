5d7bc8c18150094c1ea5459646def1dea9c5ce8c
Colin Sheridan <C.Sheridan24@nuigalway.ie>
2020-03-26 02:49:04 +0000

    Created two methods for Fees 1.Pass 2.Fail

--- NUnitTests/MethodTests.cs
+++ NUnitTests/MethodTests.cs
@@ -1,6 +1,7 @@
 using NUnit.Framework;
 using DbAssignment6;
 using System;
+using System.Collections.Generic;
 
 
 namespace NUnitTests
@@ -8,26 +9,34 @@ namespace NUnitTests
     [TestFixture]
     public class MethodTests
     {
+
         [SetUp]
         public void Test1()
         {
-            
         }
-
         [Test]
-        public void TestPermit()
+        public void TestFees()
         {
-            
 
             BusinessMethods Bus = new BusinessMethods();
 
-            Bus.CalculatePermits();
-            Console.WriteLine("Testing for Permit Status")
+            double Cost = Bus.AdditionalFees(500);
             
-
-            
-            
-            
+            Console.WriteLine("Testing Additional Fees");
+            Assert.AreEqual(550, Cost); 
         }
+
+        [Test]
+
+        public void TestFailFees()
+        {
+            BusinessMethods Bus = new BusinessMethods();
+            double cost = Bus.AdditionalFees(400);
+            Console.WriteLine("Testing Additional Fees");
+             Assert.AreEqual(460, cost);
+
+        }
+
+       
     }
 }
